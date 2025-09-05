40c0203fac986daec49e45872d475f4313745e93
Jordan Kelloway <106637327+JKells99@users.noreply.github.com>
2025-09-05 09:21:09 -0230

    Add GymProduct class with properties and methods; include basic test

--- src/main/java/com/keyin/GymProduct.java
+++ src/main/java/com/keyin/GymProduct.java
@@ -1,4 +1,27 @@
 package com.keyin;
 
 public class GymProduct {
+    private int id;
+    private String name;
+
+    public GymProduct(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/test/java/com/keyin/GymProductTest.java
+++ src/test/java/com/keyin/GymProductTest.java
@@ -1,4 +1,8 @@
 package com.keyin;
 
 public class GymProductTest {
+
+    public void testGymProduct() {
+        System.out.println("GymProduct test passed");
+    }
 }
